1ebc2c98a2251bc80071d8646b094695b1002ff4
Offeh Godfrey <offehuzezi09@gmail.com>
2023-11-12 22:36:50 +0100

    added semantic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Nav from './Components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Homepage</h2>
-    </div>
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
