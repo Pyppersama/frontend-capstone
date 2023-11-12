b3369c452ac519e5325b00fdbf800bb0cac5009f
Offeh Godfrey <offehuzezi09@gmail.com>
2023-11-12 23:31:52 +0100

    navbar added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Logo from '../Assets/Logo.svg'
+import './style.css'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+        <div>
+            <img src={Logo} alt='Littlelemon Logo'/>
+        </div>
+        <div>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Menu</a></li>
+                <li><a href='#'>Reservation</a></li>
+                <li><a href='#'>Order Online</a></li>
+                <li><a href='#'>Login</a></li>
+            </ul>
+        </div>
+    </nav>
   )
 }
 
