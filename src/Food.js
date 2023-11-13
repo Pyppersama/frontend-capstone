d712d0f4b3af1d6d835fddbb95815e7950a6311f
Offeh Godfrey <offehuzezi09@gmail.com>
2023-11-13 14:02:19 +0100

    main-specials section added

---
+++ src/Food.js
@@ -0,0 +1,28 @@
+import Bruchetta from '../src/Assets/Bruchetta.jpg';
+import GreekSalad from '../src/Assets/greek salad.jpg';
+import LemonDessert from '../src/Assets/lemon dessert.jpg'
+
+
+export const FOOD = [
+    {
+        id: 1,
+        name: 'Bruchetta',
+        price: 12.99,
+        description: 'assa aas aa asdsd d asd aa  aas saa a adaddaad aa as  aa ds fsfsdfsdfsdf ae af ada fae afa a af s fsf f s sdf s gf  s sd sdf sdfs d eweee.',
+        foodImage: Bruchetta
+    },
+    {
+        id: 2,
+        name: 'Greek Salad',
+        price: 10.99,
+        description: 'assa aas aa asdsd d asd aa  aas saa a adaddaad aa as  aa ds fsfsdfsdfsdf ae af ada fae afa a af s fsf f s sdf s gf  s sd sdf sdfs d eweee.',
+        foodImage: GreekSalad
+    },
+    {
+        id: 3,
+        name: 'Lemon Dessert',
+        price: 7.99,
+        description: 'assa aas aa asdsd d asd aa  aas saa a adaddaad aa as  aa ds fsfsdfsdfsdf ae af ada fae afa a af s fsf f s sdf s gf  s sd sdf sdfs d eweee.',
+        foodImage: LemonDessert
+    }
+]
