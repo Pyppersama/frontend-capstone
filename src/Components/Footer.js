ea20e156558719d598d3b641ffdcbef53e623958
Offeh Godfrey <offehuzezi09@gmail.com>
2023-11-13 16:32:38 +0100

    footer section added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import './style.css'
+import FooterImage from '../Assets/footerImage.jpg'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className='footer-inner'>
+            <div className='footer-image'>
+                <img src={FooterImage} alt='footer image'/>
+            </div>
+            <div className='footer-about'>
+                <h5>
+                    About
+                </h5>
+                <p>
+                    assa aas aa asdsd d asd aa aas saa a  afa a af s fsf f s sdf s gf s sd sdf sdfs d eweee.
+                </p>
+            </div>
+            <div className='footer-contact'>
+                <h5>
+                    Contact
+                </h5>
+                <p>
+                    assa aas aa fae afa a af s fsf f s sdf s gf s sd sdf sdfs d eweee.
+                </p>
+            </div>
+            <div className='footer-link'>
+                <h5>
+                    Link
+                </h5>
+                <p>
+                    assa aas aa asdsd d asd aa aas saa a adaddaad aa aafa a af s fsf f s sdf s gf s sd sdf sdfs d eweee.
+                </p>
+            </div>
+        </div>
+    </footer>
   )
 }
 
